3af6163d36395aa5971fed19196af7fcbb423173
Michael Young <micyouna@amazon.com>
2024-10-25 15:23:50 -0400

    support i18n in storybook

--- docs/landing-page/.storybook/preview.tsx
+++ docs/landing-page/.storybook/preview.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { Preview } from "@storybook/react";
 import { withThemeByClassName } from "@storybook/addon-themes";
+import "../src/i18n/client.tsx";
 import "../src/index.css";
 
 const preview: Preview = {
@@ -23,6 +24,9 @@ export const decorators = [
     },
     defaultTheme: "dark",
   }),
+  // (Story) => <I18nextProvider i18n={i18n}>
+  //   <Story />
+  // </I18nextProvider>,
 ];
 
 export default preview;
